c1efbb4441e77735482b3fadc768f26728389319
LoranOkk <107807830+LoranOkk@users.noreply.github.com>
2022-06-19 19:04:02 +0200

    Update Hello world Test.c

--- Hello world Test.c
+++ Hello world Test.c
@@ -7,6 +7,6 @@ int main()
    printf("Steijn is hier ook geweest");
 printf("Have no fear, Mohammed is hier");
 printf("Jurriaan is aan het wachten tot de rest zijn naam invoegd. \n");
-
+printf("Loran is hier ook geweest, groetjess. \n");
    return 0;
 }
