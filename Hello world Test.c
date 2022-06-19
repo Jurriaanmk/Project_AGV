c5b222d5b4e9d78f121398913985f712a51d60f8
MoosCrebolder2000 <105500202+MoosCrebolder2000@users.noreply.github.com>
2022-06-19 23:31:02 +0200

    Update Hello world Test.c

--- Hello world Test.c
+++ Hello world Test.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main() 
 {
-   // idereen weet wel hoe een printf statement werkt neem ik aan
+   // iedereen weet wel hoe een printf statement werkt neem ik aan
    printf("Hello, World! \n");
    printf("Groetjes Jurriaan katsman \n");
    printf("Steijn is hier ook geweest");
-printf("Have no fear, Mohammed is hier");
-printf("Jurriaan is aan het wachten tot de rest zijn naam invoegd. \n");
-printf("Loran is hier ook geweest, groetjess. \n");
+   printf("Moos ook.");
+   printf("Have no fear, Mohammed is hier");
+   printf("Jurriaan is aan het wachten tot de rest zijn naam invoegd. \n");
+   printf("Loran is hier ook geweest, groetjess. \n");
+   
    return 0;
 }
